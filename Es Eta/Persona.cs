03a17783c3d903486d5d6bd194dfb6bc5d8f5744
MariannaBazzani <merionbaz@gmail.com>
2024-01-23 17:37:04 +0100

    Aggiungere i file di progetto.

---
+++ Es Eta/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Eta
+{
+    internal class Persona
+    {
+        public int AnnoDiNascita { get; set; }
+        
+        public Persona() { }
+        public Persona(int AnnoDiNascita)
+        {
+            this.AnnoDiNascita = AnnoDiNascita;
+        }
+
+        public int CalcolaEta(int AnnoDiNascita)
+        {
+            int Oggi = DateTime.Now.Year;
+            int AnniOggi =
+            return AnniOggi;
+        }
+    }
+}
